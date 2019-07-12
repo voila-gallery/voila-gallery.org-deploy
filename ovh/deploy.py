8eb7a49cb3986e87a9ba8ac36065ba10d25ee81d
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-07-12 22:49:43 +0200

    Add server polling to check for readiness

--- ovh/deploy.py
+++ ovh/deploy.py
@@ -1,7 +1,9 @@
 import argparse
+import time
 import uuid
 
 import openstack
+import requests
 
 # FIXME: change prefix to voila-gallery- or make this configurable
 GALLERY_PREFIX = "voici-gallery-"
@@ -9,6 +11,26 @@ IMAGE = "Ubuntu 19.04"
 # FIXME: switch to b2-30
 FLAVOR = "b2-7"
 USER_DATA_FILE = "./user-data.sh"
+POLL_TIMEOUT = 20 * 60
+POLL_INTERVAL = 30
+
+
+def poll_server(cloud, server_id):
+    tries = POLL_TIMEOUT // POLL_INTERVAL
+    for i in range(tries):
+        try:
+            server = cloud.get_server_by_id(server_id)
+            addr_ipv4 = next(
+                (addr for addr in server.addresses["Ext-Net"] if addr["version"] == 4)
+            )
+            addr = addr_ipv4["addr"]
+            url = f"http://{addr}/api"
+            requests.get(url, timeout=10, verify=False)
+        except Exception as e:
+            print("sleep", POLL_INTERVAL)
+            time.sleep(POLL_INTERVAL)
+        else:
+            break
 
 
 def create_server(cloud, tag):
@@ -16,14 +38,14 @@ def create_server(cloud, tag):
         user_data = f.read()
 
     instance_suffix = uuid.uuid4().hex[:6]
-    cloud.create_server(
+    server = cloud.create_server(
         image=IMAGE,
         flavor=FLAVOR,
         userdata=user_data.format(ref=tag),
         name=f"{GALLERY_PREFIX}{tag}-{instance_suffix}",
     )
 
-    # FIXME: poll /api until ready
+    poll_server(cloud, server.id)
 
     # FIXME: add to an existing floating ip
 
@@ -36,8 +58,8 @@ def stop_servers(cloud, servers):
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        '--tag',
-        help='Suffix to add to the instance name (git commit hash)',
+        "--tag",
+        help="Suffix to add to the instance name (git commit hash)",
         required=True,
     )
     args = argparser.parse_args()
