e8b6ad44381f322ce08238d6759f95772d364264
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-07-12 14:52:59 +0200

    Improve deploy.py structure

--- ovh/deploy.py
+++ ovh/deploy.py
@@ -8,36 +8,51 @@ GALLERY_PREFIX = "voici-gallery-"
 IMAGE = "Ubuntu 19.04"
 # FIXME: switch to b2-30
 FLAVOR = "b2-7"
+USER_DATA_FILE = "./user-data.sh"
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument(
-    '--tag',
-    help='Suffix to add to the instance name (git commit hash)',
-    required=True,
-)
-args = argparser.parse_args()
 
-cloud = openstack.connect()
+def create_server(cloud, tag):
+    with open(USER_DATA_FILE) as f:
+        user_data = f.read()
 
-servers = cloud.list_servers()
-galleries = [server for server in servers if server.name.startswith(GALLERY_PREFIX)]
+    instance_suffix = uuid.uuid4().hex[:6]
+    cloud.create_server(
+        image=IMAGE,
+        flavor=FLAVOR,
+        userdata=user_data.format(ref=tag),
+        name=f"{GALLERY_PREFIX}{tag}-{instance_suffix}",
+    )
 
-# Create the new instance
-with open('./user-data.sh') as f:
-    user_data = f.read()
+    # FIXME: poll /api until ready
 
-instance_suffix = uuid.uuid4().hex[:6]
-cloud.create_server(
-    image=IMAGE,
-    flavor=FLAVOR,
-    userdata=user_data.format(ref=args.tag),
-    name=f"{GALLERY_PREFIX}{args.tag}-{instance_suffix}",
-)
+    # FIXME: add to an existing floating ip
 
-# FIXME: poll /api until ready
 
-# FIXME: add to an existing floating ip
+def stop_servers(cloud, servers):
+    for server in servers:
+        cloud.delete_server(server.name, wait=True)
 
-# Shutdown old servers
-for gallery in galleries:
-    cloud.delete_server(gallery.name, wait=True)
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '--tag',
+        help='Suffix to add to the instance name (git commit hash)',
+        required=True,
+    )
+    args = argparser.parse_args()
+
+    cloud = openstack.connect()
+
+    servers = cloud.list_servers()
+    galleries = [server for server in servers if server.name.startswith(GALLERY_PREFIX)]
+
+    # setup a new voila-gallery instance
+    create_server(cloud, args.tag)
+
+    # Shutdown old servers
+    stop_servers(cloud, galleries)
+
+
+if __name__ == "__main__":
+    main()
