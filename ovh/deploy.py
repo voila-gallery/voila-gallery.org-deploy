edd6e46eac541c71127ae52fe19b9d7cfab0d489
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-07-13 15:48:22 +0200

    Split server polling into active and ready

--- ovh/deploy.py
+++ ovh/deploy.py
@@ -12,8 +12,8 @@ IMAGE = "Ubuntu 19.04"
 # FIXME: switch to b2-30
 FLAVOR = "b2-7"
 USER_DATA_FILE = "./user-data.sh"
-POLL_TIMEOUT = 20 * 60
 POLL_INTERVAL = 30
+POLL_MAX_TIMEOUT = 20 * 60
 
 logger = logging.getLogger(__name__)
 
@@ -24,23 +24,42 @@ def list_servers(cloud):
     return [server for server in servers if server.name.startswith(GALLERY_PREFIX)]
 
 
-def poll_server(cloud, server_id):
-    tries = POLL_TIMEOUT // POLL_INTERVAL
-    for i in range(tries):
-        try:
-            logger.info("Check server is ACTIVE: %s", server_id)
-            server = cloud.get_server_by_id(server_id)
-            addr_ipv4 = next(
-                (addr for addr in server.addresses["Ext-Net"] if addr["version"] == 4)
-            )
-            addr = addr_ipv4["addr"]
-            url = f"http://{addr}/api"
-            logger.info("Poll %s", url)
-            requests.get(url, timeout=10, verify=False)
-        except Exception as e:
-            time.sleep(POLL_INTERVAL)
-        else:
-            break
+def retry(interval=POLL_INTERVAL, max_timeout=POLL_MAX_TIMEOUT):
+    def wrap(func):
+        def wrapped(*args, **kwargs):
+            tries = max_timeout // interval
+            for _ in range(tries):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    time.sleep(interval)
+                else:
+                    break
+
+        return wrapped
+
+    return wrap
+
+
+@retry(interval=10, max_timeout=300)
+def poll_server_active(cloud, server_id):
+    logger.info("Check server is ACTIVE: %s", server_id)
+    server = cloud.get_server_by_id(server_id)
+    if server.status != "ACTIVE":
+        raise Exception("Not ACTIVE")
+    logger.info("Server %s is ACTIVE", server_id)
+
+
+@retry()
+def poll_server_ready(cloud, server_id):
+    server = cloud.get_server_by_id(server_id)
+    addr_ipv4 = next(
+        (addr for addr in server.addresses["Ext-Net"] if addr["version"] == 4)
+    )
+    addr = addr_ipv4["addr"]
+    url = f"http://{addr}/api"
+    logger.info("Poll %s", url)
+    requests.get(url, timeout=10, verify=False)
 
 
 def create_server(cloud, tag):
@@ -89,7 +108,8 @@ def main():
     galleries = list_servers(cloud)
 
     server = create_server(cloud, args.tag)
-    poll_server(cloud, server.id)
+    poll_server_active(cloud, server.id)
+    poll_server_ready(cloud, server.id)
     # FIXME: add to an existing floating ip
     stop_servers(cloud, galleries)
 
